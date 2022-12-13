cd15de30759e93eeb97314bd428ce1c9d5135660
es <mascoeskim@gmail.com>
2022-12-14 08:11:02 +0900

    boardList

--- board/src/main/java/com/misa/board/controller/boardController.java
+++ board/src/main/java/com/misa/board/controller/boardController.java
@@ -2,6 +2,7 @@ package com.misa.board.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,5 +23,10 @@ public class boardController {
 		System.out.println("제목"+board.getTitle());
 		boardService.write(board);
 		return "";
+	}
+	@GetMapping("/board/list")
+	public String boardList(Model model) {
+		model.addAttribute("list", boardService.boardList());
+		return "boardList";
 	}
 }
--- board/src/main/java/com/misa/board/service/BoardService.java
+++ board/src/main/java/com/misa/board/service/BoardService.java
@@ -1,5 +1,7 @@
 package com.misa.board.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,5 +14,9 @@ public class BoardService 	{
 	private BoardRepository boardRepository;
 	public void write(Board board) {
 		boardRepository.save(board);
+	}
+
+	public List<Board> boardList() {
+		return boardRepository.findAll();
 	}
 }
